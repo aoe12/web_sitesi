752929de1b7ff97a43f675052b3317cd1328b7ed
aoe12 <79836014+aoe12@users.noreply.github.com>
2021-04-17 15:39:47 +0300

    Add files via upload

---
+++ 17 nisan birleşmiş son /website/views.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def anasayfa():
+    return render_template("index.html")
+
+@views.route('/anasayfa2')
+@login_required
+def anasayfa2():
+    return render_template("index2.html", user=current_user)
+
+@views.route('/hakkinda')
+def hakkinda():
+    return render_template("hakkinda.html")
+
+@views.route('/hakkinda2')
+@login_required
+def hakkinda2():
+    return render_template("hakkinda2.html", user=current_user)
+
+@views.route("/hastaliklar")
+def hastaliklar():
+    return render_template("hastaliklar.html")
+
+@views.route("/hastaliklar2")
+@login_required
+def hastaliklar2():
+    return render_template("hastaliklar2.html", user=current_user)
+
+@views.route("/sulama+bilgileri")
+def sulamabilgileri():
+    return render_template("sulamasecimleri.html", user=current_user)
